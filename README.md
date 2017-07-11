b4e3ac4b415fad3b1e87700849a71940f1251cd2
amir <amir@soluto.com>
2017-07-11 10:45:51 +0300

    updated app and readme

--- README.md
+++ README.md
@@ -1,25 +1,24 @@
 ## python-flask-docker-sklearn-template
+A simple example of python api for real time machine learning.
+On init, a simple linear regression model is created and saved on machine. On request arrival for prediction, the simple model is loaded and returing prediction.
+
 # requirements  
 docker installed
 
-# docker:
+
+# Run on docker  
 docker build . -t <some tag name>
 detached : docker run -p 3000:5000 -d <some tag name>
 interactive (recommended for debug): docker run -p 3000:5000 -it <some tag name>
 
 
-
-
-
-
-
-
-
-
+# Run locally  
 python -m venv env
 env\Scripts\Activate
-windows:
-python -m pip install -r ./requirements_windows.txt
-
-linux:
 python -m pip install -r ./requirements.txt
+python app.py
+
+
+# Use sample api  
+127.0.0.1:3000/isAlive
+127.0.0.1:3000/prediction/api/v1.0/some_prediction?f1=4&f2=4&f3=4
--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 from flask import Flask
 from flask import request
 import pandas as pd
+from sklearn import linear_model
+import pickle
 
 # creating and saving some model
 reg_model = linear_model.LinearRegression()
-reg_model.fit([[1,1,5], [2,2,5], [3,3,1]], [0,0,1])
-pickle.dump(reg_model, open('./model/some_model.pkl', 'wb'))
+reg_model.fit([[1.,1.,5.], [2.,2.,5.], [3.,3.,1.]], [0.,0.,1.])
+pickle.dump(reg_model, open('some_model.pkl', 'wb'))
 
 app = Flask(__name__)
 
@@ -14,25 +16,15 @@ app = Flask(__name__)
 def index():
     return "true"
 
-@app.route('/prediction/api/v1.0/getFromBody', methods=['POST'])
+@app.route('/prediction/api/v1.0/some_prediction', methods=['GET'])
 def get_prediction():
-    body = request.get_json()
-    loaded_model = pickle.load('./model/some_model.pkl', 'rb')
-
-    messages = pd.DataFrame(body)
-    modelFromRepo = modelRepository.load('./model/')    
-    # make sure all emotions exist
-    for k in ['sentiment', 'fear', 'anger', 'disgust', 'joy', 'sadness']:
-        if k not in messages.columns:
-            messages[k] = float('NaN')
-        messages[k] = messages[k].astype('float64')
-    session = messagesToSession.transform(messages, ['sessionId', 'isCustomer'])
-    # fill none values with mean
-    session = session.fillna(modelFromRepo['mean'])
-    # normalize 
-    session_normalized = (session - modelFromRepo['mean']) / (modelFromRepo['max'] - modelFromRepo['min'])
-    prediction = modelFromRepo['model'].predict_proba(session_normalized[modelFromRepo['features']])[:,1][0]
+    feature1 = float(request.args.get('f1'))
+    feature2 = float(request.args.get('f2'))
+    feature3 = float(request.args.get('f3'))
+    loaded_model = pickle.load(open('some_model.pkl', 'rb'))
+    prediction = loaded_model.predict([[feature1, feature2, feature3]])
     return str(prediction)
-
+   
 if __name__ == '__main__':
-    app.run(port=5000,host='0.0.0.0')
+    app.run(port=5000,host='0.0.0.0')        
+    # app.run(debug=True)
